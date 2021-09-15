97c98d373ddfb61ccddae625871c027b579f6306
Nicolas Burnett <nicolasandrewburnett@gmail.com>
2021-09-15 23:43:33 +0000

    Hotfix Pull

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 let express = require('express');
 let path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, './build')));
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
-
-app.listen(80);
+const PORT = process.env.PORT || 8080
+app.listen(PORT);
