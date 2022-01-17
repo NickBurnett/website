0ba5c2a6e23adc2d3f3ff8be21efbec9594be36c
Nicolas Andrew Burnett <nicolasandrewburnett@gmail.com>
2022-01-16 23:58:29 -0600

    fix(server): fixed build path

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 let express = require('express');
 let path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, '../build')));
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 8080
