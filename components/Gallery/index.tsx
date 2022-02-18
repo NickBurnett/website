efbd378a0a25c1e1d5c51378257d172273321331
Nicolas A. Burnett <nicolasandrewburnett@gmail.com>
2022-02-17 18:27:34 -0600

    feat(gallery): finalized gallery styles

--- components/Gallery/index.tsx
+++ components/Gallery/index.tsx
@@ -10,17 +10,24 @@ export interface GalleryProps {
   items: GalleryItem[]
 }
 export default function Gallery({ title, items }: GalleryProps) {
-  const [selectedTab, setTab] = useState('Project');
-  const tabs = items.map((i) => <div key={i.name} className={styles.tab} onClick={() => setTab(i.name)}>
-    <div className={styles.tabSelect} style={selectedTab === i.name ? { opacity: 1 } : {}} />
+  const [selectedTab, setTab] = useState(0);
+  const tabs = items.map((i, index) => <div key={i.name} className={styles.tab} onClick={() => setTab(index)}>
+    <div className={styles.tabSelect} style={selectedTab === index ? { opacity: 1 } : {}} />
     <div>{i.name}</div>
   </div>)
+  const cur = items[selectedTab]
+  const stack = cur.stack.map((i) => <div key={i} className={styles.stackItem} onClick={() => window.open(`https://google.com/search?q=${i}`, '_target')}>{i}</div>)
+  const desc = cur.description.map((i) => <div key={i} className={styles.description}>{i}</div>)
   return (
     <div className={styles.gallery}>
       <div className={styles.title}>{title}</div>
       <div className={styles.content}>
         <div className={styles.tabs}>{tabs}</div>
-        <div className={styles.info}></div>
+        <div className={styles.info}>
+          <div className={styles.name}>{cur.name}</div>
+          <div className={styles.stack}>{stack}</div>
+          {desc}
+        </div>
       </div>
     </div>
   )
