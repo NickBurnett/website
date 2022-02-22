9d2d204c67f826ae7705bb77802a052ef9ae987c
Nicolas Andrew Burnett <nicolasandrewburnett@gmail.com>
2022-02-22 01:40:05 -0600

    feat(cicd): added full terraform configuration

--- main.tf
+++ main.tf
@@ -4,3 +4,126 @@ provider "aws" {
 resource "aws_ecr_repository" "image_repo" {
   name = "website"
 }
+resource "aws_ecs_cluster" "cluster" {
+  name = "website"
+}
+resource "aws_ecs_task_definition" "task" {
+  family                   = "website-task"
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "website-task",
+      "image": "${aws_ecr_repository.image_repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  cpu                      = 256
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+}
+resource "aws_default_vpc" "default_vpc" {}
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "us-east-2a"
+}
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "us-east-2b"
+}
+resource "aws_ecs_service" "website-service" {
+  name            = "website-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tg.arn
+    container_name   = aws_ecs_task_definition.task.family
+    container_port   = 3000
+  }
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
+    assign_public_ip = true
+    security_groups  = ["${aws_security_group.svc_sg.id}"]
+  }
+}
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+resource "aws_alb" "load_balancer" {
+  name               = "website-lb"
+  load_balancer_type = "application"
+  subnets            = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
+  security_groups    = ["${aws_security_group.lb_sg.id}"]
+}
+resource "aws_security_group" "svc_sg" {
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = ["${aws_security_group.lb_sg.id}"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "lb_sg" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_lb_target_group" "tg" {
+  name        = "website-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_default_vpc.default_vpc.id
+  health_check {
+    matcher = "200,301,302"
+    path    = "/"
+  }
+}
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_alb.load_balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
